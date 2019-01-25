a503f00df8b84c21dfccdf8974b7c4379e31939b
Julien Trannoy <julien.trannoy@etu.univ-nantes.fr>
2019-01-25 15:49:26 +0100

    Modification des fichiers json / partie fetch du js

--- ville-saint-nazaire1/public/js/example.js
+++ ville-saint-nazaire1/public/js/example.js
@@ -3,26 +3,55 @@
 // No need for window.onload event here since we are using the def attribute
 // when loading our scripts
 
-// Load a dummy json file using the fetch API
-fetch('data/dummy.json')
+// Map des circuits et des lieux
+var map_circuits = new Map();
+var map_lieux = new Map();
+// infos sur les circuits
+var liste_infos_circuit = charger_donnees('nom-circuit-info');
+
+// Fonction pour charger la liste des noms de fichier à récupérer
+function charger_donnees(lien){
+  return fetch('data/' + lien + '.json')
     // this promise will be fulfilled when the json fill will be
     .then(function (response){
-        // if we could load the resource, parse it
-        if( response.ok )
-            return response.json();
-        else // if not, send some error message as JSON data
-            return {data: "JSON file not found"};
-
-    })
-    // in case of invalid JSON (parse error) send some error message as JSON data
-    .catch( function (error){
+      // if we could load the resource, parse it
+      if( response.ok )
+        return response.json();
+      else // if not, send some error message as JSON data
+        return {data: "JSON file not found"};
+      })
+      // in case of invalid JSON (parse error) send some error message as JSON data
+      .catch( function (error){
         return {data: "Invalid JSON"};
+      })
+      // this promise will be fulfilled when the json will be parsed
+      .then(function (data) {
+        return data;
+      });
+}
+
+//Fonction pour remplir les maps avec les infos des fichiers récupérés
+async function construire_map(map, folder, lien, indice) {
+  let liste_interne = await charger_donnees(lien);
+  liste_interne.forEach(function(element) {
+  //console.log(element['nom circuit']);
+  fetch(folder + element[indice] + '.json')
+    .then(function (response) {
+      if( response.ok )
+        return response.json();
+      else
+        return {data: "JSON file not found"};
     })
-    // this promise will be fulfilled when the json will be parsed
-    .then(function (json) {
-        document.querySelector('#data')
-            .textContent = json.data;
-    });
+    .then( function (json) {
+        map.set(element[indice], json);
+        // Ajouter de l'affichage ici
+    })
+    .catch( function (error) {
+      return {data: "Invalid JSON"};
+    })
+  });
+}
+
 
 
 function en_route(){
@@ -32,7 +61,6 @@ function en_route(){
   document.getElementsByClassName("bouton_accueil")[0].addEventListener("click", go);
 }
 
-
 function go(){
 
   document.getElementById("accueil").style.display="none";
@@ -41,3 +69,7 @@ function go(){
   document.getElementById("dial_carte_generale").style.display="block";
 
 }
+
+// Appel des fonctions pour charger les maps en mémoire 
+construire_map(map_circuits, 'data/trace-circuit-json/', 'nom-circuit', 'nom circuit');
+construire_map(map_lieux, 'data/trace-lieux-json/', 'nom-lieux', 'nom lieu');
