d1fe4b43f122a68ddd8759d398408eb47b9a5309
Bryan <bryanhardy@hotmail.fr>
2019-01-29 16:02:07 +0100

    Ajout des animations

--- euradio/public/js/slider.js
+++ euradio/public/js/slider.js
@@ -1,3 +1,8 @@
+var valeurRadio;
+var hasChanged = false;
+var radios = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'sed','Al', 'eiusmod','Euradio', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'aliqua','Ut','Coucou','On','Continu','jusqu'];
+
+
 $('#draggable').draggable({
   axis: "x",
   grid: [ 27, 20 ],
@@ -9,15 +14,32 @@ $('#draggable').draggable({
   containment: "parent"
 });
 
-var radios = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'sed','Al', 'eiusmod','Euradio', 'tempor', 'incididunt', 'ut', 'labore', 'et', 'dolore', 'magna', 'aliqua','Ut','Coucou','On','Continu','jusqu'];
-
 function changer_radio(xPos){
   var position = xPos;
   var valeur = parseInt(((parseInt(xPos) - 547)/27))+7; //Valeur réelle
   $( ".imagesPage1 p" ).text(radios[valeur]);
   console.log((valeur));
-  if(valeur >20){
-    document.getElementById('imgBackground').src="img/image.png";
+  if(!hasChanged){
+    valeurRadio = valeur%10+1;
+    document.getElementById('imgBackground').src="img/RadioFm/Radiofm_fréquence_"+valeurRadio+".png";
+  }else{
+    valeurRadio = valeur%7+1;
+    document.getElementById('imgBackground').src="img/RadioRnt/Radiornt_img_"+valeurRadio+".png";
+  }
+
+  if( !hasChanged &&valeur >18){
+    hasChanged =true;
+    //document.getElementById('imgBackground').src="img/rntradioRNT.png";
+
+    //document.getElementById('conteneurRadio').style.width= "40%";
+    //document.getElementById('conteneurRadio').style.marginTop= "6%";
+    //document.getElementById('conteneurRadio').style.marginBottom= "5%";
+
+    document.getElementById('sliderBackground').src="img/slider_rnt_ok.png";
+
+    document.getElementById('conteneurLigne').style.width= "50%";
+    document.getElementById('conteneurLigne').style.margin= "auto";
+    document.getElementById('conteneurLigne').style.marginTop= "-1%";
     //document.getElementById('imgBackground').marginRight="5px";
     $('#draggable').draggable({
       axis: "x",
@@ -29,7 +51,7 @@ function changer_radio(xPos){
       },
       containment: "parent"
     });
-    document.getElementById('nomRadio').style.top="65%";
-    document.getElementById('nomRadio').style.left="50%";
+    //document.getElementById('nomRadio').style.top="65%";
+    //document.getElementById('nomRadio').style.left="50%";
   }
 }
