185f6706129e9712ca86f1bd435ae40a002ef064
Romain <romandrieux29@gmail.com>
2019-01-29 10:21:58 +0100

    Modifs: slide et animations

--- air-pdl2/public/js/animation.js
+++ air-pdl2/public/js/animation.js
@@ -1,13 +1,58 @@
 
-function my() {
-    var box = document.getElementById('box');
-    var inter=setInterval(function () {
-        var xSpeed = 8;
-        var x = parseInt(box.style.left) + xSpeed;
-        //给动画设置边界
-        box.style.left = x + 'px';
-        if (x > 1500) {     box.style.left = 0 + 'px';clearInterval(inter);}
-    }, 1);
 
+var animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-}
+var currentIndex = $('div.active').index() + 1;
+$('#carousel-example-generic').on('slid.bs.carousel', function() {
+    currentIndex = $('div.active').index() + 1;
+
+    if(currentIndex===1) {
+        $('#perso').removeClass('translate');
+        $('#voiture').removeClass('animated bounce bounceOutRight');
+        $('#nuage').removeClass('fadein');
+    }
+
+    if(currentIndex===2) {
+        $('#perso').toggleClass('translate').one(animationend,
+            function(){
+            $('#voiture').addClass('animated bounce').one(animationend,
+                function(){
+                    $(this).addClass('animated bounceOutRight');
+                    $('#nuage').addClass('animated zoomIn').one(animationend,
+                        function(){
+                            $(this).removeClass('animated zoomIn');
+                            $(this).addClass('fadein');
+                        });
+                });
+        });
+    }
+
+    if(currentIndex===3) {
+        $('#nuage').addClass('translate2');
+    }
+});
+
+$('#carousel-example-generic').bind('wheel', function(e){
+    if(e.originalEvent.wheelDelta > 0) {
+        $(this).carousel('next');
+    }
+    else{
+        $(this).carousel('prev');
+    }
+
+    if(currentIndex===1){
+        $('#cache1').addClass('animated slideOutUp').one(animationend,
+            function(){
+                $(this).removeClass('animated slideOutUp');
+            });
+        $('#cache2').addClass('animated slideOutLeft').one(animationend,
+            function(){
+                $(this).removeClass('animated slideOutLeft');
+            });
+    }
+
+    if(currentIndex===2){
+        $('#nuage').removeClass('fadein');
+    }
+
+});
