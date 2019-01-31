098828ce39319bbfc6829a26cc79ae055a1b91e8
Julien <administrateur@asusrog.home>
2019-01-31 19:37:02 +0100

    Mise à jour des images / ecran 2 / cartes

--- ville-saint-nazaire1/public/js/script.js
+++ ville-saint-nazaire1/public/js/script.js
@@ -58,12 +58,14 @@ async function en_avant_toute(){
 }
 
 function d_accord(){
-  document.getElementById("presentation").style.display="none";
-  document.getElementById("presentation2").style.display="block";
+  document.getElementById("oval1").setAttribute("src", "img/oval_non_actif.svg");
+  document.getElementById("oval2").setAttribute("src", "img/oval_actif.svg");
+  document.getElementById("bulle_presentation").setAttribute("src", "img/bulle_2.png");
+  document.getElementById("bouton_presentation").setAttribute("onclick", "curseurs()");
 }
 
 function curseurs(){
-  document.getElementById("presentation2").style.display="none";
+  document.getElementById("presentation").style.display="none";
   document.getElementById("page_personnalisation").style.display="block";
 }
 
@@ -92,23 +94,26 @@ function actualiser_decor() {
   var num_theme = parseInt(document.getElementById("num_theme").value);
   /* Mise a jour suivant la distance */
   if (dist < 16.6)
-    document.getElementById("velo_curseurs").setAttribute("src", "img/velo_court.png");
+    document.getElementById("velo_curseurs").setAttribute("src", "img/leon_velo_court.png");
   else if (dist > 23.3)
-    document.getElementById("velo_curseurs").setAttribute("src", "img/velo_long.png");
+    document.getElementById("velo_curseurs").setAttribute("src", "img/leon_velo_long.png");
   else
-    document.getElementById("velo_curseurs").setAttribute("src", "img/velo_moyen.png");
+    document.getElementById("velo_curseurs").setAttribute("src", "img/leon_velo_moyen.png");
   /* Mise a jour suivant le theme */
   if(num_theme == 1){
     document.getElementById("socle_curseurs").setAttribute("src", "img/socle_foret.png");
     document.getElementById("page_personnalisation").style.backgroundImage = "url('img/fond_slider_foret.png')";
+    document.getElementById("objet_curseurs").setAttribute("src", "img/objet_foret.png");
   }
   else if(num_theme == 2){
     document.getElementById("socle_curseurs").setAttribute("src", "img/socle_musee.png");
     document.getElementById("page_personnalisation").style.backgroundImage = "url('img/fond_slider_musee.png')";
+    document.getElementById("objet_curseurs").setAttribute("src", "img/objet_musee.png");
   }
   else {
     document.getElementById("socle_curseurs").setAttribute("src", "img/socle_mer.png");
     document.getElementById("page_personnalisation").style.backgroundImage = "url('img/fond_slider_mer.png')";
+    document.getElementById("objet_curseurs").setAttribute("src", "img/objet_mer.png");
   }
 }
 
@@ -120,13 +125,31 @@ function c_est_parti(){
   
   document.getElementById("page_carte").style.display="block";
   //console.log(theme);
-
+  var nb_circuit = 0;
   selection_circuit(dist, theme)
   .then(function(value){
     value.forEach(function(element){
       var numero = element["numero"];
       document.getElementById("circuit-0" + numero).style.display="block";
+      nb_circuit = nb_circuit + 1;
     })
+    document.getElementById("texte_leon_circuit").innerHTML = "J'ai trouvé " + nb_circuit + " parcours qui pourraient vous plaire !";
+    switch (theme) {
+    case 'Forêt / Nature':
+      document.getElementById("socle_carte").setAttribute("src", "img/socle_foret.png");
+    break;
+    case 'Plage / Bord de Mer':
+      document.getElementById("socle_carte").setAttribute("src", "img/socle_mer.png");
+    break;
+    default:
+      document.getElementById("socle_carte").setAttribute("src", "img/socle_musee.png");
+    }
+    if (dist < 16.6)
+      document.getElementById("leon_carte").setAttribute("src", "img/leon_velo_court.png");
+    else if (dist > 23.3)
+      document.getElementById("leon_carte").setAttribute("src", "img/leon_velo_long.png");
+    else
+      document.getElementById("leon_carte").setAttribute("src", "img/leon_velo_moyen.png");
   });
 }
 
