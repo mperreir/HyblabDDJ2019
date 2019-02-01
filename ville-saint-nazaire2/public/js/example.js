6007d96d75fe3228069283c03000aacd6cb919bc
HoumairOthman <45081244+HoumairOthman@users.noreply.github.com>
2019-02-01 11:41:30 +0100

    Update example.js

--- ville-saint-nazaire2/public/js/example.js
+++ ville-saint-nazaire2/public/js/example.js
@@ -1,25 +1,24 @@
-'use strict';
-
-// No need for window.onload event here since we are using the def attribute
-// when loading our scripts
-
-// Load a dummy json file using the fetch API
-fetch('data/dummy.json')
-    // this promise will be fulfilled when the json fill will be
-    .then(function (response){
-        // if we could load the resource, parse it
-        if( response.ok )
-            return response.json();
-        else // if not, send some error message as JSON data
-            return {data: "JSON file not found"};
-
-    })
-    // in case of invalid JSON (parse error) send some error message as JSON data
-    .catch( function (error){
-        return {data: "Invalid JSON"};
-    })
-    // this promise will be fulfilled when the json will be parsed
-    .then(function (json) {
-        document.querySelector('#data')
-            .textContent = json.data;
+$(document).ready(function() {
+    
+    //activate wow.js
+     new WOW().init();
+  
+    //activate fullpage.js
+    $('#fullpage').fullpage({
+      scrollBar: true,
+      navigation: true,
+      navigationTooltips: ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4'],
+      loopBottom: true,
+      sectionSelector: 'section'
     });
+  
+  //apply color to each section from array
+  int = -1;
+  color_array = ['#1abc9c','#c0392b','#9b59b6','#3498db','#f1c40f','#16a085'];
+
+  $('section').each(function(){
+    int++
+    $(this).addClass('grid-item-' + int).css('background-color', color_array[int]);
+  });
+  
+});
