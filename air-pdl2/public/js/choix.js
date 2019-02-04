40146fa5e8941138086822db2276dde40d7df1ac
Romain <romandrieux29@gmail.com>
2019-02-04 06:47:24 +0100

    Version_finale_1

--- air-pdl2/public/js/choix.js
+++ air-pdl2/public/js/choix.js
@@ -1,59 +1,47 @@
-var passageRural = false;
-var passageVille = false;
-var passageTraffic = false;
+
 
 $('#rural').on('click',
     function () {
-        passageRural = true;
         $('#carousel-example-generic').carousel(3);
     });
 
 $('#ville').on('click',
     function () {
-        passageVille = true;
         $('#carousel-example-generic').carousel(4);
     });
 
 $('#trafic').on('click',
     function () {
-        passageTraffic = true;
         $('#carousel-example-generic').carousel(5);
     });
 
-$('.button').on('click',
+$('.retour').on('click',
     function () {
         $('#carousel-example-generic').carousel(2);
     });
 
-$('.svga1Button').on('click',
-    function () {
-        if($('div.active').index() === 3){
-            passageRural = true;
-            if(passageVille && passageTraffic){
-                $('#carousel-example-generic').carousel(6);
-            }
-            else{
-                $('#carousel-example-generic').carousel(4);
-            }
-        }
+$('.nextArrow').on('click', function () {
+    $('#carousel-example-generic').carousel('next');
+});
 
-        if($('div.active').index() === 4){
-            passageVille = true;
-            if(passageTraffic && passageRural){
-                $('#carousel-example-generic').carousel(6);
-            }
-            else {
-                $('#carousel-example-generic').carousel(5);
-            }
-        }
+$('#nextArrow2').on('click', function () {
+    $('#carousel-example-generic').removeClass('carousel-fade');
+    $('#carousel-example-generic').carousel(6);
+});
 
-        if($('div.active').index() === 5){
-            passageTraffic = true;
-            if(!passageRural){
-                $('#carousel-example-generic').carousel(3);
-            }
-            else{
-                $('#carousel-example-generic').carousel(6);
-            }
-        }
-    });
+$('.backArrow').on('click', function () {
+    $('#carousel-example-generic').carousel('prev');
+});
+
+$('#backArrow2').on('click', function () {
+    $('#carousel-example-generic').carousel(2);
+});
+
+$('#recommandations').on('click', function () {
+    $('#carousel-example-generic').addClass('carousel-fade');
+    $('#carousel-example-generic').carousel('next');
+});
+
+$('#retour-slide-2').on('click',function () {
+    $('.fond').hide();
+});
