60cad1ea3c956fa97d6fa284d829d72aab562b04
Maxim <maxim.le-cloerec@etu.univ-nantes.fr>
2019-02-04 13:26:10 +0100

    slider image
    hop

--- euradio/public/js/imageslider.js
+++ euradio/public/js/imageslider.js
@@ -1,8 +1,8 @@
 $("#before_after_images").mouseup(function(){
-  if ($("div.twentytwenty-handle").position().left < 20){
+  if ($("div.twentytwenty-handle").position().left < 100){
     var a = "img/fm_spécialisations.png";
     document.getElementById("differences").src = a;
-  }else if ($("div.twentytwenty-handle").position().left > 390) {
+  }else if ($("div.twentytwenty-handle").position().left > 300) {
     var b = "img/rnt_spécialisations.png";
     document.getElementById("differences").src = b;
   }
