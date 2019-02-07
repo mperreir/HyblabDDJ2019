cd6ffb99473bb74ff00ced3ab5f7f3545e299fc0
Oukache <Ayasouk@users.noreply.github.com>
2019-02-07 10:29:19 +0100

    Update example.js

--- music-data/public/js/example.js
+++ music-data/public/js/example.js
@@ -3,7 +3,7 @@ let datafollowers=[];
 let datatracks=[];
 let Tabpays=[];
 let arr=[];
-$("#selectcontry").hide();
+//$("#selectcontry").hide();
 $('#bloginformations').hide();
 
 // fonction de trie des elements
@@ -55,18 +55,22 @@ function GetAllData()
           .then(function (json) {
 
                 datatracks=json;
-                $('#menu').append('<div class="rectangle-path4"></div>');
-                $('#menu').append('<button class="rectangle-path" > Nombre de bars</button>');
-                $('#menu').append('<button  class="rectangle-path"> Tracks</button>');
-                $('#menu').append('<button class="rectangle-path2" id="followers"  value='+key+'>Followers</button>');
-                $('#bottonderetour').append('<button class="left" id="retour"  value="Genres">Genres</button>');
+
+
                AfficherCursseur("#trackspourungenre",datatracks);
+               $('#menu').append('<div id="nomdegenre">'+key+'</div>');
+               $('#menu').append('<div id="retour" class="rectangle-path">Genres</div>');
+               $('#menu').append('<button class="rectangle-path" > Nombre de bars</button>');
+              $('#menu').append('<div  class="rectangle-path">Tracks</div>');
+               $('#menu').append('<button class="rectangle-path" id="followers"  value='+key+'>Followers</button>');
+               //$('#bottonderetour').append('<button class="left" id="retour"  value="Genres">Genres</button>');
+
                creehistogram("#trackspourungenre",json,20);
 
                creehistogramReflet("#trackspourungenreReflet",json,20);
 //bottonderetour
                 $(document).on("click", "#retour", function(){
-                $('.slidecontainer').empty();
+                $('.slidec').empty();
                 $('#geresmusicales').show();
                 $("#trackspourungenre").empty();
                 $("#trackspourungenreReflet").empty();
@@ -75,7 +79,7 @@ function GetAllData()
                 $("#bottonderetour").empty();
                 $("#bloginformations").empty();
                 $("#bloginformations").hide();
-                $("#selectcontry").hide();
+                //$("#selectcontry").hide();
 
 
 
@@ -109,23 +113,32 @@ function GetAllData()
 
 
                       //je fais cette affichage juste pour que les choses soit claire, mais apres on doit la changer par le design et le mode d'affichage que l'on veut
-                      AfficherCursseur("#followerspourungenre",datafollowers);
+                     var pres_histo = document.createElement('DIV');
+                     pres_histo.setAttribute('class',"pres-histo");
+                     pres_histo.append("Survole les niveaux sonors, filtre et découvrezles blogs de ce genre.\n" +
+                         "Sélectionne et compare afin de trouver l’artiste qui te fera vibrer.");
+                     $('#menu').append("<div id='nomdegenre'>"+key+"</div>");
+                     $('#menu').append(pres_histo);
+                     AfficherCursseur("#followerspourungenre",datafollowers);
+                      $('#menu').append('<div id="nomdegenre">'+key+'</div>');
+                      $('#menu').append('<div id="retour" class="rectangle-path">Genres</div>');
+                      $('#menu').append('<button class="rectangle-path">Nombre de Bars</button>');
+                      $('#menu').append('<button id="tracks" class="rectangle-path" value='+key+'> Tracks</button>');
+                      $('#menu').append('<button class="rectangle-path">Followers</button>');
+                      //$('#bottonderetour').append('<button class="left" id="retour"  value="Genres">Genres</button>');
                       creehistogram("#followerspourungenre",data,20);
                       creehistogramReflet("#followerspourungenreReflet",data,20);
-                     $('#menu').append('<div class="rectangle-path4"></div>');
-                     $('#menu').append('<button class="rectangle-path3">Nombre de Bars</button>');
-                     $('#menu').append('<button id="tracks" class="rectangle-path" value='+key+'> Tracks</button>');
-                     $('#menu').append('<button class="rectangle-path">Followers</button>');
-                     $('#bottonderetour').append('<button class="left" id="retour"  value="Genres">Genres</button>');
+
                       //$('#menu').append("<button id='retour' class='buttonmenu'> Retour à l'ensemble des Genres </button>");
 
                       $(document).on("click", "#retour", function(){
-                       $('.slidecontainer').empty();
+                       $('.slidec').empty();
                        $('#geresmusicales').show();
                        $("#followerspourungenre").empty();
                        $("#followerspourungenreReflet").empty();
                        $('#artistespourungenre').empty();
                        $("#menu").empty();
+
                        $("#bottonderetour").empty();
                        $("#bloginformations").empty();
                        $("#bloginformations").hide();
@@ -162,8 +175,124 @@ function GetAllData()
 
                            });
                   }
+
+// les genres  accompagnés par leurs nombre de blogs
+function GetGenres()
+{
+    $("#selectcontry").hide();
+    fetch('/BlogParGenre/')
+        .then(function (response){
+            if( response.ok )
+            {
+                return response.json();
+            }
+            else
+                return {data: "JSON file not found"};
+        })
+        .catch( function (error){
+            return {data: "Invalid JSON"};
+        })
+
+        .then(function (json) {
+            console.log(json);
+            //$('.slidecontainer').hide();
+            var i=0;
+            var j=0;
+            // ajout du titre de la page
+            var title = document.createElement('div');
+            title.setAttribute("class", "presentation-choix-genre");
+            title.append("Clique sur ton influence et explore ses blogs");
+            $('#geresmusicales').append(title);
+            $.each( json, function( key, val ) {
+                // ici je vais affcher l'ensemble des genre accompagnés chacun par le nombre de blog qui le contient
+                //$('#geresmusicales').append("<button id= "+ escape(key)+ ">"+key+"</button>");
+                //$('#geresmusicales').append(" le nombre de blogs est " + val +  "<br>");
+                //$('#onde-genres').append("<div id='genre-+escape(key) class='onde-genre'>key</div>");
+                //$('#onde-genres').append("<div id="+ escape(key)+ "class='onde-genre'>"+key+"<object type='image/svg+xml' data='img/choix-genres/'"+key+".svg class='onde-genre-svg'</div>");
+                //key ==> le id de genre
+                //$('#geresmusicales').append("<button id= "+ escape(key)+ ">"+key+"</button>");
+                //le nombre de blog pour ce genre
+                //$('#geresmusicales').append(" le nombre de blogs est " + val +  "<br>");
+
+                // creation d'une section pour chaque genre
+                var genre = document.createElement("DIV");
+                genre.setAttribute("id", escape(key));
+
+                // creation du svg correspondant au genre
+                var svg = document.createElement('OBJECT');
+                svg.setAttribute("class","onde-test");
+                svg.setAttribute("type","image/svg+xml");
+                svg.setAttribute("data", "img/choix-genres/"+key+".svg");
+                svg.style.width="100%";
+                svg.style.height="100%";
+                // creation d'une div pour le titre du genre
+                var titre = document.createElement("DIV");
+                titre.setAttribute("class", "titre-choix-genre");
+                titre.append(key);
+
+                // style de la section genre
+                genre.style.position="absolute";
+                genre.style.top = 33+(20*j)+"%";
+                genre.style.left = 18+(20*i)+"%";
+                genre.style.width="10%";
+                genre.style.height="auto";
+
+                // ajout des sections complementaires
+                genre.append(svg);
+                genre.append(titre);
+
+                // Last arrangement
+                //$('#genres .fp-tableCell').append(genre);
+                $('#geresmusicales').append(genre);
+                //var k="#" + key;
+
+                var k="#"+key.replace("%20","\\ ");
+                console.log("k",k);
+                i++;
+                if(i==4){
+                    j++;
+                    i=0;
+                }
+                /*$("#choix-"+key).select("svg").setAttribute("onmouseover",function(key){
+                    this.style.width=this.style.width+10+"%";
+                    this.style.height=this.style.height+10+"%";
+                    this.style.color = "#FF00000";
+                });*/
+
+                //var svg = $("#"+escape(key)).select('svg').append("TESTTTTTT");
+                /*elem.hover(function(){
+                    console.log("hover sur un choix de genre");
+                    //this.setAttribute("class","titre-choix-genre-hover");
+                    this.style.width=this.style.width+10+"%";
+                    this.style.height=this.style.height+10+"%";
+                    this.style.color = "#FF00000";
+                });*/
+
+                $(document).on("click", "#"+key, function(){
+                    console.log("appel fonction");
+                    tracksdesblog(key);
+
+                    $('#geresmusicales').hide();
+
+                });
+
+            });
+            $("#choix-Funk").select(".onde-test").style.width="60%";
+            var defss = $('#Funk').select('defs');
+            var anime = document.createElement('ANIME');
+            /*anime.setAttribute["animate id"] = "Funk";
+            anime.setAttribute["attributeName"]="top";
+            anime.setAttribute["attributeType"]="XML";
+            anime.setAttribute["from"]="60";
+            anime.setAttribute["to"]="10";
+            anime.setAttribute["begin"]="5s";
+            anime.setAttribute["dur"]="4s";
+            anime.setAttribute["fill"]="freeze";*/
+            defss.append(anime);
+        });
+}
     // les genres  accompagnés par leurs nombre de blogs
-    function GetGenres()
+    function GetGenres2()
     {
       $("#selectcontry").hide();
       fetch('/BlogParGenre/')
@@ -198,6 +327,8 @@ function GetAllData()
                  console.log("k",k);
 
                  $(document).on("click", k, function(){
+
+
                   console.log("appel fonction");
                   tracksdesblog(key);
 
@@ -260,13 +391,14 @@ function GetAllData()
 
         var x = $(this).attr("value");
         console.log("x",x);
-          $('.slidecontainer').empty();
+          $('.slidec').empty();
           $("#bloginformations").hide();
           $("#bloginformations").empty();
           $('#trackspourungenre').empty();
           $('#artistespourungenre').empty();
           $("#trackspourungenreReflet").empty();
           $("#menu").empty();
+
           $("#bottonderetour").empty();
           $("#bloginformations").empty();
           //$("#selectcontry").show();
@@ -278,13 +410,14 @@ function GetAllData()
   $(document).on("click", "#tracks", function(){
       var x = $(this).attr("value");
       console.log("x",x);
-      $('.slidecontainer').empty();
+      $('.slidec').empty();
       $("#bloginformations").empty();
       $("#bloginformations").hide();
       $('#followerspourungenre').empty();
       $('#artistespourungenre').empty();
         $('#followerspourungenreReflet').empty();
       $('#menu').empty();
+
       $("#bottonderetour").empty();
       $("#bloginformations").empty();
       tracksdesblog(x);
@@ -296,9 +429,10 @@ $(document).on("click", "#artistes", function(){
     console.log("x",x);
       $("#bloginformations").hide();
       $("#bloginformations").empty();
-    $('#followerspourungenre').empty();
-    $('#trackspourungenre').empty();
-    $("#menu").empty();
+      $('#followerspourungenre').empty();
+      $('#trackspourungenre').empty();
+
+      $("#menu").empty();
       $("#trackspourungenreReflet").empty();
       $("#followerspourungenreReflet").empty();
 
@@ -417,19 +551,47 @@ function AfficherCursseur(zone,data)
   var dataactuelle;
   let rowData = {};
 
-  $("#selectcontry").show();
-  $('.slidecontainer').append('<div class="rectangle-path"><p id="valeurdeslider1">20</p><input type="range" id="slider" class="slider" min="0" max="157" value="20"></div>');
-  $('.slidecontainer').append('<div class="rectangle-path2"><p id="valeurdeslider2">0</p><input type="range" id="slider2" class="slider" min="0" max="23000" value="0"></div>');
-  $('.slidecontainer').append('<div class="rectangle-path3"><p id="valeurdeslider3">0</p><input type="range" id="slider3" class="slider" min="0" max="10000" value="0"></div>');
+  //$("#selectcontry").show();
+
+
+/*  $('#menu').append('<div class="rectangle-path4"></div>');
+  $('#menu').append('<button class="rectangle-path" > Nombre de bars</button>');
+  $('#menu').append('<button  class="rectangle-path"> Tracks</button>');
+  $('#menu').append('<button class="rectangle-path" id="followers" >Followers</button>');
+  $('#bottonderetour').append('<button class="left" id="retour"  value="Genres">Genres</button>');
+*/
+  $('.slidec').append('<div class="selectcontry"><select id="selectcontry" size="2" multiple></select>');
+  $("#selectcontry").append('<option value="All" selected="selected">All</option>');
+      arr.push("All");
+     for(var i=0;i<Tabpays.length;i++)
+     {
+
+
+       $("#selectcontry").append('<option value='+Tabpays[i]+'>'+Tabpays[i] + '</option>');
+      //  console.log("genre",Tabpays[i]);
+     }
+     $('.slidec').append('</div>')
+
+     //$("#selectcontry").hide();
+
+     console.log("pays tab",Tabpays);
+  $('.slidec').append('<div class="rectangle-path"><p id="valeurdeslider1">20</p><input type="range" id="slider" class="slider" min="0" max="157" value="20"></div>');
+  $('.slidec').append('<div class="rectangle-path"><p id="valeurdeslider2">0</p><input type="range" id="slider2" class="slider" min="0" max="23000" value="0"></div>');
+  $('.slidec').append('<div class="rectangle-path"><p id="valeurdeslider3">0</p><input type="range" id="slider3" class="slider" min="0" max="10000" value="0"></div>');
   $('#slider2').on('change', function () {
+    $(zone).empty();
+    var reflet=zone + "Reflet";
+    $(reflet).empty();
   $('#valeurdeslider2').empty();
   $('#valeurdeslider2').append($(this).val());
-    $('#bloginformations').hide();
+  $('#bloginformations').hide();
 
   traitementsurlesdonnees(data,zone);
   });
   $('#slider3').on('change', function () {
-
+    $(zone).empty();
+    var reflet=zone + "Reflet";
+    $(reflet).empty();
   $('#valeurdeslider3').empty();
   $('#valeurdeslider3').append($(this).val());
   $('#bloginformations').hide();
@@ -437,9 +599,12 @@ function AfficherCursseur(zone,data)
 });
   $('#slider').on('change', function () {
     //la je devrais mettre une copie de data
+    $(zone).empty();
+    var reflet=zone + "Reflet";
+    $(reflet).empty();
     $('#valeurdeslider1').empty();
     $('#valeurdeslider1').append($(this).val());
-      $('#bloginformations').hide();
+    $('#bloginformations').hide();
     traitementsurlesdonnees(data,zone);
 });
 $('#selectcontry').change(function(){
@@ -451,6 +616,9 @@ $('#selectcontry').change(function(){
   arr.push(element);
 }
 );
+$(zone).empty();
+var reflet=zone + "Reflet";
+$(reflet).empty();
 // j'appelle ici le truc de traitementsurlesdonnees
 traitementsurlesdonnees(data,zone);
 
@@ -481,11 +649,11 @@ function creehistogramReflet(id,data1,val)
       var t=data.length -20;
        data.splice(0,t);
        nbbins=20;
-       alert("Le nombre de bar que vous voulez afficher est supérieur aux nombre de blog, on va vous afficher la valeur par défaut des bars, que 20 bars");
+      // alert("Le nombre de bar que vous voulez afficher est supérieur aux nombre de blog, on va vous afficher la valeur par défaut des bars, que 20 bars");
     }
     else{
       nbbins=data.length;
-      alert("Le nombre de bar que vous voulez afficher est supérieur aux nombre de blog et superieur à la valeur par défaut 20, on va vous afficher le nombre de bars possible")
+      //alert("Le nombre de bar que vous voulez afficher est supérieur aux nombre de blog et superieur à la valeur par défaut 20, on va vous afficher le nombre de bars possible")
     }
     console.log(nbbins);
 
@@ -553,18 +721,18 @@ function Getpays()
           return {data: "Invalid JSON"};
       })
       .then(function (json) {
-        $("#selectcontry").append('<option value="All" selected="selected">All</option>');
-        arr.push("All");
+        //$("#selectcontry").append('<option value="All" selected="selected">All</option>');
+        //arr.push("All");
            for(var i=0;i<json.length;i++)
            {
 
              Tabpays.push(json[i]);
-             $("#selectcontry").append('<option value='+json[i]+'>'+json[i] + '</option>');
+             //$("#selectcontry").append('<option value='+Tabpays[i]+'>'+Tabpays[i] + '</option>');
             //  console.log("genre",Tabpays[i]);
            }
 
 
-           $("#selectcontry").hide();
+           //$("#selectcontry").hide();
 
            console.log("pays tab",Tabpays);
       });
@@ -573,13 +741,11 @@ function Getpays()
 }
 function traitementsurlesdonnees(data,zone)
 {
-
+var reflet=zone + "Reflet";
 var nbbins=parseInt($('#slider').val());
 var mintracks=parseInt($('#slider2').val());
 var minfollowers=parseInt($('#slider3').val());
-$(zone).empty();
-var reflet=zone + "Reflet";
-$(reflet).empty();
+
 var copiedata=JSON.parse(JSON.stringify(data));
   for(var i=0;i<copiedata.length;i++)
   {
